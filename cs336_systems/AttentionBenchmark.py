a831d0a3a8c2c8a6e18a34461e520ec3dc71e91b
Jingyuan He <jingyuanhe@Jingyuans-Mac-mini.local>
2026-08-08 09:17:42 -0400

    add attention benchmark

---
+++ cs336_systems/AttentionBenchmark.py
@@ -0,0 +1,45 @@
+import torch
+from cs336_basics.model import scaled_dot_product_attention
+import time
+
+start = time.perf_counter()
+
+
+# num_layers for this model is 32
+D = [16, 32, 64, 128]
+T = [256, 1024, 4096, 8192, 16384]
+
+# warmup
+Q = torch.randn((8, 256, 16), requires_grad=True, device="cuda")
+K = torch.randn((8, 256, 16), requires_grad=True, device="cuda")
+V = torch.randn((8, 256, 16), requires_grad=True, device="cuda")
+mask = None
+for _ in range(10):
+    y = scaled_dot_product_attention(Q, K, V, mask, device="cuda")
+    loss = y.sum()
+    loss.backward()
+
+curr = start
+with torch.autograd.graph.saved_tensors_hooks():
+    for d in D:
+        for t in T:
+            Q = torch.randn((8, t, d), requires_grad=True, device="cuda")
+            K = torch.randn((8, t, d), requires_grad=True, device="cuda")
+            V = torch.randn((8, t, d), requires_grad=True, device="cuda")
+            mask = None
+            for _ in range(100):
+                y = scaled_dot_product_attention(Q, K, V, mask, device="cuda")
+                torch.cuda.synchronize()
+                forward_end = time.perf_counter()
+            print(f"Time taken: {forward_end - curr} seconds")
+            print(f"Memory allocated: {torch.cuda.memory_allocated()}")
+        
+            for _ in range(100):
+                loss = y.sum()
+                loss.backward(retain_graph=True)
+                torch.cuda.synchronize()
+                backward_end = time.perf_counter()
+            print(f"Time taken: {backward_end - forward_end} seconds")
+            curr = backward_end
+
+
